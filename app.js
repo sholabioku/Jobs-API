45ba09626ab78ec7b6d7808d5ac044b6ed52feaa
sholabioku <bilushonline@gmail.com>
2021-10-21 14:32:53 +0100

    Protect jobs route with authentication middleware

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ const app = express();
 const connectDB = require('./db/connect');
 
 const authRouter = require('./routes/auth');
-const jobs = require('./routes/jobs');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const authenticateUser = require('./middleware/authentication');
 
 app.use(express.json());
 // extra packages
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobs);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
