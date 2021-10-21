cc55e73f303b0236f5a23c53dfc9d9d11f895006
sholabioku <bilushonline@gmail.com>
2021-10-21 17:22:32 +0100

    Add functionality of getJobs method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,7 +4,8 @@ const Job = require('../models/Job');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  res.send('Get all jobs');
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
