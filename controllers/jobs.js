030ffef91e5f86fca02798413d6379b001300dc1
sholabioku <bilushonline@gmail.com>
2021-10-21 17:59:39 +0100

    Add functionality of getJob method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,16 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send('Get single job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
