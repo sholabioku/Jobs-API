1db7876f4dc4abdf93fb1159fbbbff28b92b58db
sholabioku <bilushonline@gmail.com>
2021-10-23 20:58:49 +0100

    prepare app for deployment

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Jobs API');
+});
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
