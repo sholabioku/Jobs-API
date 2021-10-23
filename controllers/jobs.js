f547ebc0c63e03c7b2a8a7e5e5701ae67f1cdaa2
sholabioku <bilushonline@gmail.com>
2021-10-23 08:09:58 +0100

    Add functionality of deleteJob method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -52,7 +52,16 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndRemove({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
