37c1730babef7e83857e170ab6b1b0fb1d7f0308
sholabioku <bilushonline@gmail.com>
2021-10-23 20:38:18 +0100

    Add header security

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const app = express();
 
 const connectDB = require('./db/connect');
@@ -13,8 +18,18 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticateUser = require('./middleware/authentication');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
